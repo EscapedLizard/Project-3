f59fa80d6bc423442ef39e8cbeb48d53d077bc32
EscapedLizard <bethanymixon101@gmail.com>
2026-03-05 10:44:12 -0500

    2nd commit

--- src/connect4/Game.java
+++ src/connect4/Game.java
@@ -1,4 +1,26 @@
 package connect4;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Game {
+
+    //Create the frame
+    JFrame frame = new JFrame("Connect 4");
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    //Create the JPanel
+    JPanel panel = new JPanel(new GridLayout(7,7));
+
+//    int row = 0;
+//    int col = 0;
+//    while (){
+//        JButton button = new JButton(new ImageIcon());
+//        col++;
+//    }
+
+
+    //Adds a label to the top of JPanel
+    JLabel title = new JLabel("Player 1's turn");
+
 }
